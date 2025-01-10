2b71ee164e5aca1665c98f5467b1e11a4fa28c85
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 16:38:12 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ function rank() {
 	"author": {
             "@type": "Person",
             "name": "Aia88bet : Slot Member Baru di Kasih Menang Dan Situs Slot 100 New Member",
-            "reviewBody": "Aia88bet : Slot Member Baru di Kasih Menang Dan Situs Slot 100 New Member, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!"
+            "reviewBody": "Aia88bet menawarkan peluang luar biasa bagi para pemain baru dengan program slot yang menarik. Sebagai situs slot terpercaya, Aia88bet memberikan bonus besar untuk member baru dengan menjanjikan kemenangan yang menguntungkan. Para pemain yang baru bergabung akan merasakan sensasi bermain dengan peluang menang yang lebih tinggi"
           }
         },
     "offers": {
@@ -47,6 +47,22 @@ function rank() {
     };
   }
 
+function histats() {
+    return {
+      __html: `
+        var _Hasync= _Hasync|| [];
+        _Hasync.push(['Histats.start', '1,4921003,4,5,172,25,00010000']);
+        _Hasync.push(['Histats.fasi', '1']);
+        _Hasync.push(['Histats.track_hits', '']);
+        (function() {
+        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+        hs.src = ('//s10.histats.com/js15_as.js');
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+        })();
+      `,
+    };
+  };
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -55,7 +71,6 @@ export default function RootLayout({
   return (
     <html lang="id" data-browser="chrome">
       <head>
-        <meta charSet="utf-8" />
         <title>Aia88bet : Slot Member Baru di Kasih Menang Dan Situs Slot 100 New Member</title>
         <meta name="description" content="Aia88bet menawarkan peluang luar biasa bagi para pemain baru dengan program slot yang menarik. Sebagai situs slot terpercaya, Aia88bet memberikan bonus besar untuk member baru dengan menjanjikan kemenangan yang menguntungkan. Para pemain yang baru bergabung akan merasakan sensasi bermain dengan peluang menang yang lebih tinggi" />
         <meta name="keywords" content="Aia88bet, Slot Member Baru di Kasih Menang, Situs Slot 100 New Member, Situs Resmi Pragmatic Slot, Website Resmi Pragmatic Slot, Deposit Via E Wallet, Situs Judi Online, Deposit Pulsa" />
@@ -80,7 +95,7 @@ export default function RootLayout({
         <meta name="distribution" content="global" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
         <meta property="og:title" content="Aia88bet : Slot Member Baru di Kasih Menang Dan Situs Slot 100 New Member" />
-        <meta property="og:description" content="Aia88bet : Slot Member Baru di Kasih Menang Dan Situs Slot 100 New Member, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta property="og:description" content="Aia88bet menawarkan peluang luar biasa bagi para pemain baru dengan program slot yang menarik. Sebagai situs slot terpercaya, Aia88bet memberikan bonus besar untuk member baru dengan menjanjikan kemenangan yang menguntungkan. Para pemain yang baru bergabung akan merasakan sensasi bermain dengan peluang menang yang lebih tinggi" />
         <meta property="og:url" content={nameDomain} />
         <meta property="og:site_name" content="aia88bet" />
         <meta property="og:locale" content="id_ID" />
@@ -91,12 +106,12 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="aia88bet" />
         <meta name="twitter:title" content="Aia88bet : Slot Member Baru di Kasih Menang Dan Situs Slot 100 New Member" />
-        <meta name="twitter:description" content="Aia88bet : Slot Member Baru di Kasih Menang Dan Situs Slot 100 New Member, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta name="twitter:description" content="Aia88bet menawarkan peluang luar biasa bagi para pemain baru dengan program slot yang menarik. Sebagai situs slot terpercaya, Aia88bet memberikan bonus besar untuk member baru dengan menjanjikan kemenangan yang menguntungkan. Para pemain yang baru bergabung akan merasakan sensasi bermain dengan peluang menang yang lebih tinggi" />
         <meta name="twitter:image" content={nameDomain} />
         <link rel="icon" href="/favicon.ico?favicon.088e6c96.ico" sizes="256x256" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="next-size-adjust" content="" />
         <script type="application/ld+json" dangerouslySetInnerHTML={rank()} key="product-jsonld" />
+        <script dangerouslySetInnerHTML={histats()} type="text/javascript" />
       </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
